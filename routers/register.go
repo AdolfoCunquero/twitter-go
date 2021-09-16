bfcdb076e50d40813abb756140392373f848b087
AdolfoCunquero <adolfocunquero.077@gmail.com>
2021-09-16 12:25:24 -0600

    Add Json response on error

--- routers/register.go
+++ routers/register.go
@@ -6,42 +6,57 @@ import (
 
 	"github.com/AdolfoCunquero/twitter-go/db"
 	"github.com/AdolfoCunquero/twitter-go/models"
+	"github.com/AdolfoCunquero/twitter-go/utils"
 )
 
 func Register(rw http.ResponseWriter, r *http.Request) {
 	var usr models.User
+	var customErr models.Error
+
 	err := json.NewDecoder(r.Body).Decode(&usr)
 
 	if err != nil {
-		http.Error(rw, "Error en registro de usuario "+err.Error(), 400)
+		customErr.Code = 400
+		customErr.Message = "Error en registro de usuario " + err.Error()
+		utils.JSONError(rw, customErr, 400)
 		return
 	}
 
 	if len(usr.Email) == 0 {
-		http.Error(rw, "El Email de usuario es requerido", 400)
+		customErr.Code = 400
+		customErr.Message = "El Email de usuario es requerido"
+		utils.JSONError(rw, customErr, 400)
 		return
 	}
 
 	if len(usr.Password) < 6 {
-		http.Error(rw, "El Password tiene que tener minimo 6 caracteres", 400)
+		customErr.Code = 400
+		customErr.Message = "El Password tiene que tener minimo 6 caracteres"
+		utils.JSONError(rw, customErr, 400)
 		return
 	}
 	_, exists, _ := db.UserExists(usr.Email)
 
 	if exists {
-		http.Error(rw, "El email ya existe un usuario registrado con el email "+usr.Email, 400)
+		customErr.Code = 400
+		customErr.Message = "El email ya existe un usuario registrado con el email " + usr.Email
+		utils.JSONError(rw, customErr, 400)
 		return
 	}
 
 	_, status, err := db.InsertRegister(usr)
 
 	if err != nil {
-		http.Error(rw, "Ocurrio un error al registrar el usuario "+err.Error(), 400)
+		customErr.Code = 400
+		customErr.Message = "Ocurrio un error al registrar el usuario " + err.Error()
+		utils.JSONError(rw, customErr, 400)
 		return
 	}
 
-	if status {
-		http.Error(rw, "No se ha logrado registrar el usuario", 400)
+	if !status {
+		customErr.Code = 400
+		customErr.Message = "No se ha logrado registrar el usuario"
+		utils.JSONError(rw, customErr, 400)
 		return
 	}
 
